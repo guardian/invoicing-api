68539d224e629f0526488c9c09ba154f14ed52db
Mario Galic <mario.galic@guardian.co.uk>
2020-04-24 16:02:39 +0100

    Improve assertion messages

--- src/main/scala/com/gu/invoicing/refund/Program.scala
+++ src/main/scala/com/gu/invoicing/refund/Program.scala
@@ -17,21 +17,21 @@ object Program {
     val RefundInput(subscriptionName, refund, guid, _) = input
 
     val subscription          = getSubscription(subscriptionName)                              tap { subscription           => s"$subscriptionName should be retrieved"                                     assert (subscription.subscriptionNumber == subscriptionName) }
-    val balanceBeforeRefund   = getAccountBalance(subscription.accountId)
+    val balanceBeforeRefund   = getAccountBalance(subscription.accountId)                      tap { balanceBeforeRefund    => s"$balanceBeforeRefund should be recorded"                                   assert (true) }
     val invoices              = getInvoices(subscription.accountId)                            tap { invoices               => s"$subscriptionName should have at least one invoice"                        assert (invoices.nonEmpty) }
     val itemsBySubscription   = getItemsByInvoice(subscriptionName)                            tap { invoiceItemsByInvoice  => s"$subscriptionName should have at least one invoice item"                   assert (invoiceItemsByInvoice.nonEmpty) }
 
-    val (invoiceId, _, items) = decideRelevantInvoice(invoices, itemsBySubscription)           tap { case (_, invoice, _)   => s"$invoice should at least not be negative"                                  assert (invoice.Amount > 0.0) }
-    val itemAdjustments       = getInvoiceItemAdjustments(invoiceId)                           tap { _                      => s"Meaning of life should be confired"                                        assert (true) }
-    val Some(paymentId)       = getInvoicePaymentId(invoiceId)                                 tap { paymentId              => s"Invoice $invoiceId should have a payment"                                  assert (paymentId.isDefined) }
-    val adjustmentsUnrounded  = spreadRefundAcrossItems(items, itemAdjustments, refund, guid)  tap { adjustmentsUnrounded   => s"Net adjustments (un-rounded) amount should equal total refund amount"      assert (adjustmentsUnrounded.map(_.Amount).sum == refund) }
-    val adjustments           = roundAdjustments(adjustmentsUnrounded)                         tap { adjustments            => s"Net adjustments (rounded) amount should equal total refund amount "        assert (adjustments.map(_.Amount).sum == refund) }
+    val (invoiceId, _, items) = decideRelevantInvoice(invoices, itemsBySubscription)           tap { case (_, invoice, _)   => s"$invoice should be at least posted and not negative"                       assert (invoice.Amount > 0.0 && invoice.Status == "Posted") }
+    val itemAdjustments       = getInvoiceItemAdjustments(invoiceId)                           tap { _                      => s"Invoice items of $invoiceId should be retrieved"                                         assert (true) }
+    val Some(paymentId)       = getInvoicePaymentId(invoiceId)                                 tap { paymentId              => s"$invoiceId should have a payment"                                          assert (paymentId.isDefined) }
+    val adjustmentsUnrounded  = spreadRefundAcrossItems(items, itemAdjustments, refund, guid)  tap { adjustmentsUnrounded   => s"$adjustmentsUnrounded (un-rounded) should equal total $refund"             assert (adjustmentsUnrounded.map(_.Amount).sum == refund) }
+    val adjustmentsRounded    = roundAdjustments(adjustmentsUnrounded)                         tap { adjustmentsRounded     => s"$adjustmentsRounded (rounded) amount should equal total $refund"           assert (adjustmentsRounded.map(_.Amount).sum == refund) }
 
-    val refundId              = createRefundObject(refund, paymentId, guid)                    tap { _                      => s"Meaning of life should be confired"                                        assert (true) }
-    val _                     = getRefundStatus(refundId)                                      tap { refundStatus           => s"Refund $refundId should be processed"                                      assert (refundStatus == "Processed") }
-    val result                = applyRefundOverItemAdjustments(adjustments)                    tap { adjustments            => s"All $adjustments should be successful"                                     assert (adjustments.forall(_.Success)) }
-    val balanceAfterRefund    = getAccountBalance(subscription.accountId)                      tap { balanceAfterRefund     => s"Account ${subscription.accountId} balance should not change after refund." assert (balanceBeforeRefund == balanceAfterRefund) }
+    val refundId              = createRefundObject(refund, paymentId, guid)                    tap { _                      => s"Refund object for $paymentId should be created"                            assert (true) }
+    val _                     = getRefundStatus(refundId)                                      tap { refundStatus           => s"$refundId in amount of $refund should be processed"                        assert (refundStatus == "Processed") }
+    val result                = applyRefundOverItemAdjustments(adjustmentsRounded)             tap { adjustments            => s"All $adjustments should be successful"                                     assert (adjustments.forall(_.Success)) }
+    val balanceAfterRefund    = getAccountBalance(subscription.accountId)                      tap { balanceAfterRefund     => s"${subscription.accountId} balance should not change after refund"          assert (balanceBeforeRefund == balanceAfterRefund) }
 
-    RefundOutput(subscriptionName, refund, invoiceId, paymentId, adjustments, guid)
+    RefundOutput(subscriptionName, refund, invoiceId, paymentId, adjustmentsRounded, guid)
   }
 }
