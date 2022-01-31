b703d64ad032a03211e9f2284b0924d51c414ca9
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-01-31 15:21:39 +0000

    Add endpoint to refund erroneous payments

---
+++ src/main/scala/com/gu/invoicing/refundErroneousPayment/Impl.scala
@@ -0,0 +1,109 @@
+package com.gu.invoicing.refundErroneousPayment
+
+import com.gu.invoicing.common.Http
+import com.gu.invoicing.common.ZuoraAuth.{accessToken, zuoraApiHost}
+import com.gu.invoicing.refundErroneousPayment.Model._
+
+import scala.util.chaining._
+
+/**
+  * Zuora API client and implementation details
+  */
+object Impl {
+
+  def getAccountBalance(accountId: String): BigDecimal = {
+    Http(s"$zuoraApiHost/v1/accounts/$accountId")
+      .header("Authorization", s"Bearer $accessToken")
+      .asString
+      .body
+      .pipe(read[Account](_))
+      .metrics
+      .balance
+  }
+
+  def createRefundObject(amount: BigDecimal,
+                         paymentId: String,
+                         comment: String): String = {
+    Http(s"$zuoraApiHost/v1/object/refund")
+      .header("Authorization", s"Bearer $accessToken")
+      .header("Content-Type", "application/json")
+      .postData(
+        s"""
+           |{
+           |  "Amount": $amount,
+           |  "Comment": "$comment",
+           |  "PaymentId": "$paymentId",
+           |  "Type": "Electronic"
+           |}
+           |""".stripMargin)
+      .method("POST")
+      .asString
+      .body
+      .pipe(body => read[RefundResult](body))
+      .Id
+  }
+
+  def getRefundStatus(refundId: String): String = {
+    Http(s"$zuoraApiHost/v1/object/refund/$refundId")
+      .header("Authorization", s"Bearer $accessToken")
+      .asString
+      .body
+      .pipe(read[Refund](_))
+      .Status
+  }
+
+  def getBalancingInvoice(accountId: String, invoiceAmount:BigDecimal):String = {
+    val body = Http(s"$zuoraApiHost/v1/action/query")
+      .header("Authorization", s"Bearer $accessToken")
+      .header("Content-Type", "application/json")
+      .postData(s"""{"queryString": "SELECT Id, Amount, Balance, InvoiceDate, InvoiceNumber, PaymentAmount, TargetDate, Status FROM Invoice WHERE AccountId = '$accountId' AND status = 'Posted'"}""")
+      .method("POST")
+      .asString
+      .body
+    val value = body
+      .pipe(read[InvoiceQueryResult](_))
+      .records
+      .sortBy(_.InvoiceDate)
+      .reverse
+      .take(2)
+    value match {
+      case neg :: pos :: Nil if neg.Amount == -invoiceAmount && pos.Amount == invoiceAmount => neg.InvoiceNumber
+    }
+  }
+
+  def transferToCreditBalance(invoiceNumber:String, invoiceAmount:BigDecimal, comment:String):Unit =
+    Http(s"$zuoraApiHost/v1/object/credit-balance-adjustment")
+      .header("Authorization", s"Bearer $accessToken")
+      .header("Content-Type", "application/json")
+      .postData(
+        s"""
+           |{
+           |  "Amount": $invoiceAmount,
+           |  "Comment": "$comment",
+           |  "SourceTransactionNumber": "$invoiceNumber",
+           |  "Type": "Increase"
+           |}
+           |""".stripMargin)
+      .method("POST")
+      .asString
+      .body
+      .pipe(body => read[RefundResult](body))
+
+  def applyCreditBalance(balancingInvoiceNumber:String, invoiceAmount:BigDecimal, comment:String) :Unit =
+    Http(s"$zuoraApiHost/v1/object/credit-balance-adjustment")
+      .header("Authorization", s"Bearer $accessToken")
+      .header("Content-Type", "application/json")
+      .postData(
+        s"""
+           |{
+           |  "Amount": $invoiceAmount,
+           |  "Comment": "$comment",
+           |  "SourceTransactionNumber": "$balancingInvoiceNumber",
+           |  "Type": "Decrease"
+           |}
+           |""".stripMargin)
+      .method("POST")
+      .asString
+      .body
+      .pipe(body => read[RefundResult](body))
+}
---
+++ src/main/scala/com/gu/invoicing/refundErroneousPayment/README.md
@@ -0,0 +1,39 @@
+# Refund Erroneous Payment
+
+This is for refunding in cases where a payment was inadvertently taken for an invoice.
+This could be, for instance, when a cancelled sub has been left with an amount outstanding
+and then a retry attempt is made to collect the outstanding amount.
+
+### Example request
+
+Request
+
+```http
+POST /CODE/refund-erroneous-payment
+Host: <API ID>.execute-api.eu-west-1.amazonaws.com
+Content-Type: application/json
+
+{
+    "accountId": "A-S12345678",
+    "invoiceNumber": "INV123456",
+    "paymentId": "p123",
+    "invoiceAmount": 0.69
+    "comment": "???"
+}
+```
+
+Response
+
+```
+{
+    "accountId": "A123",
+    "paymentId": "P123",
+    "invoiceAmount": 0.69,
+    "invoiceNumber": "INV123456",
+    "balancingInvoiceNumber": "INV654321"
+}
+```
+
+### How to manually apply refund
+
+TODO
