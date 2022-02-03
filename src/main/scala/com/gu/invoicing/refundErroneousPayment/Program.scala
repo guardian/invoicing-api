6d0d710fa5132bfd03c0cbce21ec3d536113b0ed
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-02-03 14:29:52 +0000

    Refine program to handle many invoice payments

--- src/main/scala/com/gu/invoicing/refundErroneousPayment/Program.scala
+++ src/main/scala/com/gu/invoicing/refundErroneousPayment/Program.scala
@@ -3,16 +3,33 @@ package com.gu.invoicing.refundErroneousPayment
 import com.gu.invoicing.refundErroneousPayment.Impl._
 import com.gu.invoicing.refundErroneousPayment.Model._
 
+import java.time.LocalDate
+
 object Program {
 
   def program(input: RefundInput): RefundOutput = {
-    val RefundInput(accountId, invoiceNumber, paymentId, invoiceAmount, comment, _) = input
-    val balancingInvoiceNumber = getBalancingInvoice(accountId, invoiceAmount)
-    transferToCreditBalance(balancingInvoiceNumber, invoiceAmount, comment)
-    val refundId = createRefundObject(invoiceAmount, paymentId, comment)
-    assert(getRefundStatus(refundId) == "Processed")
-    applyCreditBalance(invoiceNumber, invoiceAmount, comment)
-    assert(getAccountBalance(accountId) == 0)
-    RefundOutput(accountId, paymentId, invoiceAmount, invoiceNumber, balancingInvoiceNumber)
+    val RefundInput(accountId, paymentDate, comment, _) = input
+    val payments = getPayments(accountId, paymentDate)
+    assert(payments.nonEmpty, "No payments")
+    val invoices = getInvoices(accountId)
+    assert(invoices.nonEmpty, "No invoices")
+    val balancingInvoice = invoices.maxBy(_.InvoiceDate)
+    assert(balancingInvoice.InvoiceDate.isBefore(paymentDate), "Bad balancing invoice date")
+    assert(
+      balancingInvoice.Amount == -payments.map(_.amount).sum,
+      "Payments don't agree with balancing invoice"
+    )
+    transferToCreditBalance(balancingInvoice.InvoiceNumber, -balancingInvoice.Amount, comment)
+    val results = payments.map(processRefund(comment))
+    assert(getAccountBalance(accountId) == 0, "Account balance isn't settled")
+    RefundOutput(accountId, results, balancingInvoice.InvoiceNumber)
+  }
+
+  def processRefund(comment: String)(payment: Payment): RefundData = {
+    assert(payment.paidInvoices.length == 1, "Payment is for multiple invoices")
+    val refundId = createRefundObject(payment.amount, payment.id, comment)
+    assert(getRefundStatus(refundId) == "Processed", "Refund hasn't been processed")
+    applyCreditBalance(payment.paidInvoices.head.invoiceNumber, payment.amount, comment)
+    RefundData(payment.paidInvoices.head.invoiceNumber, payment.amount, payment.id, refundId)
   }
 }
