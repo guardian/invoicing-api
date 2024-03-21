ad7e66c40ed4d6aa9728fca7ab3e3c44bfefb8b6
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2024-03-21 19:04:52 +0000

    update

--- src/main/scala/com/gu/invoicing/refund/RefundHandler.scala
+++ src/main/scala/com/gu/invoicing/refund/RefundHandler.scala
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse.BatchItemFa
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, SQSBatchResponse, SQSEvent}
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
-import com.typesafe.scalalogging.LazyLogging
 import cats.data.EitherT
 import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -14,13 +13,14 @@ import io.circe.parser.{decode => circeDecode}
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
-class RefundHandler  extends LazyLogging with RequestHandler[SQSEvent, Unit] {
+class RefundHandler  extends RequestHandler[SQSEvent, Unit] {
 
   override def handleRequest(input: SQSEvent, context: Context): Unit = {
     System.out.println("Handling request")
 
     val messages = input.getRecords.asScala.toList
     System.out.println(s"Received ${messages.length} messages")
+    System.out.println(s"Received the message - ${messages }")
 
     val failedMessageIds = messages.map(message => processEvent(message , context))
       .collect { case Left(messageId) => messageId }
@@ -40,13 +40,13 @@ class RefundHandler  extends LazyLogging with RequestHandler[SQSEvent, Unit] {
 
   private def processEvent(message: SQSMessage, context: Context): Either[SQSMessageId, Unit] = {
     val rawBody = message.getBody
-    logger.info(s"Processing event: $rawBody")
+    System.out.println(s"Processing event: $rawBody")
 
     val result: EitherT[Future, Error, Unit] = EitherT
       .fromEither[Future](circeDecode[APIGatewayProxyRequestEvent](rawBody))
       .leftMap[Error](error => ParseError(error.getMessage))
       .flatMap(event => {
-        logger.info(s"Event: $event")
+        System.out.println(s"Event: $event")
         event match {
           case _ => EitherT.rightT[Future, Error](())
         }
@@ -55,15 +55,15 @@ class RefundHandler  extends LazyLogging with RequestHandler[SQSEvent, Unit] {
 
     val resultEither: Either[SQSMessageId, Unit] = Try(Await.result(result.value, 20.seconds)) match {
       case Success(Right(_)) =>
-        logger.info(s"Successfully processed the event: $rawBody")
+        System.out.println(s"Successfully processed the event: $rawBody")
         Right(())
       case Success(Left(ParseError(error))) =>
-        logger.error(s"Failed to process an event due to ParseError($error)")
-        logger.error(s"Failed event was: $rawBody")
+        System.out.println(s"Failed to process an event due to ParseError($error)")
+        System.out.println(s"Failed event was: $rawBody")
         Left(message.getMessageId)
       case Failure(error) =>
-        logger.error(s"Failed to process an event due to Failure($error)")
-        logger.error(s"Failed event was: $rawBody")
+        System.out.println(s"Failed to process an event due to Failure($error)")
+        System.out.println(s"Failed event was: $rawBody")
         Left(message.getMessageId)
     }
     resultEither
